8c595077e8261d87b48a13a7e18e2b2190a028cb
이승종 <maeugel_rosh@naver.com>
2023-07-12 17:33:40 +0900

    feat: 상품주문

--- jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -46,6 +46,11 @@ public class OrderSimpleApiController {
 
         return result;
     }
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+    }
+
 
     @Data
     static class SimpleOrderDto {
