dbbe6c2f35648e537ef206a28c914b247c9991f7
이승종 <maeugel_rosh@naver.com>
2023-07-18 17:41:39 +0900

    feat: spring data JPA paging까지

--- data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -78,4 +78,27 @@ class MemberJpaRepositoryTest {
         assertThat(result.size()).isEqualTo(1);
 
     }
+
+    @Test
+    public void paging() {
+        //given
+        memberJpaRepository.save(new Member("member1", 10));
+        memberJpaRepository.save(new Member("member2", 10));
+        memberJpaRepository.save(new Member("member3", 10));
+        memberJpaRepository.save(new Member("member4", 10));
+        memberJpaRepository.save(new Member("member5", 10));
+
+        int age = 10;
+        int offset = 0;
+        int limit = 3;
+
+        //when
+
+        List<Member> findMembers = memberJpaRepository.findByPage(age, offset, limit);
+        long totalCount = memberJpaRepository.totalCount(age);
+
+        //then
+        assertThat(findMembers.size()).isEqualTo(3);
+        assertThat(totalCount).isEqualTo(5);
+    }
 }
